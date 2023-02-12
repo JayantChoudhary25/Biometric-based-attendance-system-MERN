70d2fafabf275a06169001866134f456677fb150
Nikhil Goyal <nikhilgoyalm.p@gmail.com>
2023-02-12 16:09:25 +0530

    -- dashboard enhancement

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function dashboard() {
+export default function Dashboard() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(!localStorage.getItem("user")){
+      navigate("/login");
+    }// eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
   return (
     <div>
       <h1>Welcome to dashboard</h1>
