568ee00af5663a12c58ab8400a273aff3478222e
Jay <>
2023-02-26 12:13:58 +0530

    --Get teacher by ID

--- backend/controllers/teacher.js
+++ backend/controllers/teacher.js
@@ -25,6 +25,15 @@ exports.get_all_teacher = async (req, res, next) => {
   }
 };
 
+exports.get_teacher_byID = async (req, res, next) => {
+  try {
+    const result = await Teacher.findOne({where: {_id: req.body._id}},{_id:0, teacher_name:1 , phone_number:1,email:1,subject:1,_class:1});
+    return res.json({result});
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.remove_teacher = async (req, res, next) => {
   try {
     const result = await Teacher.findByIdAndDelete({ _id: req.body._id });
@@ -39,14 +48,14 @@ exports.remove_teacher = async (req, res, next) => {
 };
 
 exports.update_teacher = async (req, res, next) => {
-    const { teacher_name, phone_number, email, subject } = req.body;
+    const { teacher_name, phone_number, email, subject, _class } = req.body;
     try {
       const result = await Teacher.updateMany({
         teacher_name,
         email,
         phone_number,
         subject,
-        class: req.body.class,
+        _class,
         where: {
             _id: req.body._id
         }
